0d2c6398169ce383c8466c15726b393b8a7a3177
Carlos Gerardo Acosta Hernández <cgah.95@gmail.com>
2020-04-15 15:44:54 -0500

    If it breaks, go back in time

--- src/viaticos/viajes/browser/static/comprobacion_reset.js
+++ src/viaticos/viajes/browser/static/comprobacion_reset.js
@@ -2,6 +2,11 @@ function confirmar(descripcion) {
     var confirmado = confirm(descripcion);
     console.log(window.location);
     if (confirmado) {
-	window.location += "/@@reset_list";
+	var win_loc = window.location.toString();
+	if(win_loc.includes("view")){
+	    win_loc = win_loc.substring(0,win_loc.length-5);
+	}
+	window.location = win_loc+"/@@reset_list";
+	//window.location += "/@@reset_list";
     }
 }
